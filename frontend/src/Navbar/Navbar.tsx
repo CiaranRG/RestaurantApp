97127ec0f3ed59ae65c98c63dacab3bea13fa8fa
Ciaran Grant <ciarangrant2@aol.co.uk>
2023-11-14 01:59:37 +0000

    Login and register modal differeniated when clicked

--- frontend/src/Navbar/Navbar.tsx
+++ frontend/src/Navbar/Navbar.tsx
@@ -4,20 +4,31 @@ import './Navbar.scss'
 
 // Importing our modal component and then the login/registration content that will go inside of it
 import LoginRegisterModal from '../Modals/LoginAndRegister/LoginAndRegister';
-import LoginPage from '../pages/AccountPages/LoginAndRegistration/LoginPage';
+import LoginForm from '../pages/AccountPages/LoginAndRegistration/LoginForm';
 import RegistrationPage from '../pages/AccountPages/LoginAndRegistration/RegistrationPage';
 
 export default function Navbar(){
 
     // Using state to track if the modal is open and what type of content is inside of it
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState('')
 
     // Creating a function for toggling the modal to be the opposite of what it Currently is
     const toggleModal = () => {
         setIsModalOpen(!isModalOpen)
         console.log(isModalOpen)
     }
-    
+
+    // Making two more to set the content to be the one that was clicked below
+    const toggleLoginModal = () => {
+        toggleModal()
+        setModalContent('login')
+    }
+    const toggleRegisterModal = () => {
+        toggleModal()
+        setModalContent('register')
+    }
+
     return(
         <>
         <nav>
@@ -40,12 +51,16 @@ export default function Navbar(){
             </div>
             <div className='rightNav'>
                 {/* <a href="">My Account</a> */}
-                <a onClick={toggleModal}>Login</a>
-                <a onClick={toggleModal}>Register</a>
+                <a onClick={toggleLoginModal}>Login</a>
+                <a onClick={toggleRegisterModal}>Register</a>
             </div>
         </nav>
-        {/* Here we use our modal component and pass the isOpen and toggleModel function, whatever is between our component will be considered the children when passed through*/}
-        <LoginRegisterModal isOpen={isModalOpen} toggleModal={toggleModal}><LoginPage/></LoginRegisterModal>
+        {/* Using a ternary operator to display the correct modal based on whats in modalContent, the children being passed in is the thing between the component (loginForm etc) */}
+        {modalContent === 'login' ? (
+        <LoginRegisterModal isOpen={isModalOpen} toggleModal={toggleModal}><LoginForm/></LoginRegisterModal>
+    ) : modalContent === 'register' ? (
+        <LoginRegisterModal isOpen={isModalOpen} toggleModal={toggleModal}><RegistrationPage/></LoginRegisterModal>
+    ) : null}
         </>
     )
 }
---
+++ frontend/src/pages/AccountPages/LoginAndRegistration/LoginForm.tsx
@@ -0,0 +1,65 @@
+import Button from '../../../Components/BookTable/Button'
+import './LoginForm.scss'
+import axios from 'axios'
+import { useEffect, useState} from 'react'
+
+// Defining Types
+type LoginInfo = {
+    username: string;
+    password: string;
+}
+
+export default function LoginForm(){
+    const [loginInfo, setLoginInfo] = useState<LoginInfo>({username: '', password: ''})
+    useEffect(()=>{
+        document.title = 'Login'
+    },[])
+    const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
+        evt.preventDefault()
+        try {
+            const response = await axios.post('http://localhost:5000/api/login', loginInfo)
+            console.log(response)
+            setLoginInfo({username: '', password: ''})
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    // Making the type to be a react change event that has either TextAreas or Inputs
+    const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        // Destructuring the values and the type so we can run a check below
+        const { name, value } = evt.target;
+        setLoginInfo(prevInfo => ({ ...prevInfo, [name]: value }));
+    }
+
+    const handleModalClick = (evt: React.FormEvent<HTMLFormElement>) => {
+        // Prevents the click event from bubbling up to the modal overlay
+        evt.stopPropagation();
+    };
+    
+    return (
+        <main className='loginFormMainContent'>
+            <div onClick={handleModalClick}>
+                <div className='loginFormDiv'>
+                <h1 className='loginHeaderText'>Login Page</h1>
+                <h2 className='loginSubheaderText'>This is the subheader text for the login page</h2>
+                    <form className='loginForm' action="" onSubmit={handleSubmit}>
+                        <div className='loginFormRow'>
+                            <label htmlFor="">
+                            Username:
+                            </label>
+                            <input type="text" name='username' value={loginInfo.username} onChange={handleChange}/>
+                        </div>
+                        <div className='loginFormRow'>
+                            <label htmlFor="">
+                            Password:
+                            </label>
+                            <input type="password" name='password' value={loginInfo.password} onChange={handleChange}/>
+                        </div>
+                        <Button text='Login'/>
+                    </form>
+                </div>
+            </div>
+        </main>
+    )
+}
