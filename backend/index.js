b9b868129c4f2b58b5f889f122d21d704d3f06b1
Ciaran Grant <ciarangrant2@aol.co.uk>
2024-06-25 15:11:04 +0100

    fixed cors mistake

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ const PORT = 5000
 // Adding in origin to allow requests from the frontend and also setting credentials to true for user authentication through cookies
 app.use(
     cors(
-        {origin: 'http://localhost:5173', credentials: true}
+
+        {origin: 'https://restaurant-app-sigma-nine.vercel.app', credentials: true}
     )
 );
 app.use(express.json());
