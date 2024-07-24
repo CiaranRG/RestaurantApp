45838e88d8e2b104776f29b2a4916fd750cab575
Ciaran Grant <Ciarangrant2@aol.co.uk>
2024-07-24 20:49:02 +0100

    Vercel deployment test

--- frontend/src/pages/AccountPages/LoginAndRegistration/RegisterForm.tsx
+++ frontend/src/pages/AccountPages/LoginAndRegistration/RegisterForm.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Button from '../../../Components/Button/Button'
 import './RegisterForm.scss'
 import ErrorMessage from '../../../Components/ErrorMessage/ErrorMessage'
@@ -17,53 +18,53 @@ type registerFormProps = {
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export default function RegisterForm({ toggleModal }: registerFormProps){
-    const [registerInfo, setRegisterInfo] = useState<RegisterInfo>({email: '', username: '', password: ''})
+export default function RegisterForm({ toggleModal }: registerFormProps) {
+    const [registerInfo, setRegisterInfo] = useState<RegisterInfo>({ email: '', username: '', password: '' })
     // When setting state with just '' typescript infers that its a string, if you want to be explicit you can add <string> like you usually would.
-    const [error, setError] = useState({show: false, message: ''})
-    const [success, setSuccess] = useState({show: false, message: ''})
+    const [error, setError] = useState({ show: false, message: '' })
+    const [success, setSuccess] = useState({ show: false, message: '' })
 
     useEffect(() => {
         // If showError is truthy do this and also set a timeout to hide the error display after 3 seconds
         if (error.show) {
             const timer = setTimeout(() => {
-                setError({show: false, message: ''});
+                setError({ show: false, message: '' });
             }, 8000); // Telling the callback to run after 8 seconds
 
             // Setting up a cleanup function to run when the component unmounts, we pass a reference to the timer we want to clear
             return () => clearTimeout(timer);
         }
     }, [error.show]); // Adding the dependencies but making sure it only changes when the show property of the object changes
-    
+
     const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
         evt.preventDefault()
         try {
             const response = await axios.post(`${apiUrl}/api/accounts`, registerInfo)
-            if (import.meta.env.MODE === 'development') {
+            if ((import.meta as any).env.MODE !== 'production') {
                 console.log(registerInfo)
             }
-            setRegisterInfo({email: '', username: '', password: ''})
+            setRegisterInfo({ email: '', username: '', password: '' })
             // Checking for a response and if the token is in the response 
             if (response.data && response.data.token) {
-                setSuccess({show: true, message: 'Your Account has Been Registered'})
+                setSuccess({ show: true, message: 'Your Account has Been Registered' })
             }
-            setError({show: false, message: ''})
+            setError({ show: false, message: '' })
             toggleModal()
         } catch (error) {
-            if (import.meta.env.MODE === 'development') {
+            if ((import.meta as any).env.MODE !== 'production') {
                 console.log('We hit the error somehow', error)
             }
             // Using AxiosError to check if the error is an axios one or some other type of error, also checking to see if there is an error response for below
-            if (axios.isAxiosError(error) && error.response){
-                if (import.meta.env.MODE === 'development') {
+            if (axios.isAxiosError(error) && error.response) {
+                if ((import.meta as any).env.MODE !== 'production') {
                     console.log(error)
                 }
                 // Checking if the error has data and message properties and if it does we put that in the error message to be used otherwise it will have a generic error message
                 if (error.response.data.error === 'Validation error') {
-                    setError({show: true, message: 'Your inputs were not valid, please try again!'})
+                    setError({ show: true, message: 'Your inputs were not valid, please try again!' })
                 } else {
                     // Setting another generic error if its not an axios error
-                    setError({show: true, message: 'An unexpected error has occurred'})
+                    setError({ show: true, message: 'An unexpected error has occurred' })
                 }
             }
         }
@@ -80,33 +81,33 @@ export default function RegisterForm({ toggleModal }: registerFormProps){
         // Prevents the click event from bubbling up to the modal overlay
         evt.stopPropagation();
     };
-    
+
     return (
         <main className='registerFormMainContent'>
             <div onClick={handleModalClick}>
-            { error.show === true ? (<ErrorMessage message={error.message}/>) : success.show === true ? (<ErrorMessage message={success.message}/>) : <div></div>}
+                {error.show === true ? (<ErrorMessage message={error.message} />) : success.show === true ? (<ErrorMessage message={success.message} />) : <div></div>}
                 <div className='registerFormDiv'>
-                <h1 className='registerHeaderText'>Create Account</h1>
+                    <h1 className='registerHeaderText'>Create Account</h1>
                     <form className='registerForm' action="" onSubmit={handleSubmit}>
                         <div className='registerFormRow'>
                             <label htmlFor="" className='registerEmailLabel'>
-                            Email: (3-30 Characters)
+                                Email: (3-30 Characters)
                             </label>
-                            <input type="email" name='email' className='registerEmailInput'  value={registerInfo.email} onChange={handleChange}/>
+                            <input type="email" name='email' className='registerEmailInput' value={registerInfo.email} onChange={handleChange} />
                         </div>
                         <div className='registerFormRow'>
                             <label htmlFor="" className='registerUsernameLabel'>
-                            Username: (3-30 Characters, No Spaces)
+                                Username: (3-30 Characters, No Spaces)
                             </label>
-                            <input type="text" name='username' className='registerUsernameInput' value={registerInfo.username} onChange={handleChange}/>
+                            <input type="text" name='username' className='registerUsernameInput' value={registerInfo.username} onChange={handleChange} />
                         </div>
                         <div className='registerFormRow'>
                             <label htmlFor="" className='registerPasswordLabel'>
-                            Password: (3-30 Characters)
+                                Password: (3-30 Characters)
                             </label>
-                            <input type="password" name='password' className='registerPasswordInput' value={registerInfo.password} onChange={handleChange}/>
+                            <input type="password" name='password' className='registerPasswordInput' value={registerInfo.password} onChange={handleChange} />
                         </div>
-                        <Button text='Register Now!'/>
+                        <Button text='Register Now!' />
                     </form>
                 </div>
             </div>
--- frontend/src/pages/AccountPages/MyAccountPages/AccountPage.tsx
+++ frontend/src/pages/AccountPages/MyAccountPages/AccountPage.tsx
@@ -11,29 +11,29 @@ type AccountPageProps = {
     onLogout: () => void
 }
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = (import.meta as any).env.VITE_API_URL;
 
-export default function AccountPage({ onLogout }: AccountPageProps){
+export default function AccountPage({ onLogout }: AccountPageProps) {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [reservations, setReservations] = useState([])
-    const [deleteAccount, setDeleteAccount] = useState({isDelete: false, text: 'Delete Account'})
+    const [deleteAccount, setDeleteAccount] = useState({ isDelete: false, text: 'Delete Account' })
     const [isLoading, setIsLoading] = useState(false)
 
     const navigate = useNavigate()
 
-    useEffect(()=>{
+    useEffect(() => {
         document.title = 'My Account'
-    },[])
+    }, [])
 
     // Using this to get the reservations data
     useEffect(() => {
         const fetchReservations = async () => {
             setIsLoading(true)
             try {
-                const response = await axios(`${apiUrl}/api/reservations`, {method: 'GET', withCredentials: true})
+                const response = await axios(`${apiUrl}/api/reservations`, { method: 'GET', withCredentials: true })
                 // We have to use .then since we cannot use any of the data until the promise has resolved 
-                    setReservations(response.data.result)
-                    setIsLoading(false)
+                setReservations(response.data.result)
+                setIsLoading(false)
             } catch (err) {
                 if (import.meta.env.MODE === 'development') {
                     console.log(err)
@@ -53,15 +53,15 @@ export default function AccountPage({ onLogout }: AccountPageProps){
     const deleteAccountRequest = () => {
         // Set up this function to delete the account and reservations associated with it
         try {
-            axios(`${apiUrl}/api/accounts`, {method: 'DELETE', withCredentials: true})
-            // We have to use .then since we cannot use any of the data until the promise has resolved 
-            .then((response) => {
-                if (response.data.message === 'Deletion Successful'){
-                    onLogout()
-                    // Using this to navigate the user back to the homepage upon account deletion
-                    navigate('/home');
-                }
-            })
+            axios(`${apiUrl}/api/accounts`, { method: 'DELETE', withCredentials: true })
+                // We have to use .then since we cannot use any of the data until the promise has resolved 
+                .then((response) => {
+                    if (response.data.message === 'Deletion Successful') {
+                        onLogout()
+                        // Using this to navigate the user back to the homepage upon account deletion
+                        navigate('/home');
+                    }
+                })
         } catch (err) {
             if (import.meta.env.MODE === 'development') {
                 console.log(err)
@@ -71,8 +71,8 @@ export default function AccountPage({ onLogout }: AccountPageProps){
     }
 
     const handleDeleteClick = () => {
-        if (deleteAccount.isDelete === false){
-            setDeleteAccount({isDelete: true, text: 'Confirm?'})
+        if (deleteAccount.isDelete === false) {
+            setDeleteAccount({ isDelete: true, text: 'Confirm?' })
         } else {
             deleteAccountRequest()
         }
@@ -91,37 +91,37 @@ export default function AccountPage({ onLogout }: AccountPageProps){
         terms_conditions: boolean,
     }
 
-    return(
+    return (
         <>
-        <main className='pageBackground'>
-            <div className='pageWrapper'>
-                {isLoading ? <h1 className='accountHeaderText'>Loading!</h1> : 
-                <>
-                    <h1 className='accountHeaderText'>My Account</h1>
-                    <h2 className='accountHeaderTextTwo'>Check Your Bookings Or Create One!</h2>
-                    <div className='accountButtonsDiv'>
-                        <Button classProp='accountPageButtons' text={deleteAccount.text} onClick={handleDeleteClick}></Button>
-                        <Button classProp='accountPageButtons' text={'Book a Table'} onClick={toggleModal}></Button>
+            <main className='pageBackground'>
+                <div className='pageWrapper'>
+                    {isLoading ? <h1 className='accountHeaderText'>Loading!</h1> :
+                        <>
+                            <h1 className='accountHeaderText'>My Account</h1>
+                            <h2 className='accountHeaderTextTwo'>Check Your Bookings Or Create One!</h2>
+                            <div className='accountButtonsDiv'>
+                                <Button classProp='accountPageButtons' text={deleteAccount.text} onClick={handleDeleteClick}></Button>
+                                <Button classProp='accountPageButtons' text={'Book a Table'} onClick={toggleModal}></Button>
+                            </div>
+                            <h2 className='bookingsTitle'>Bookings Below!</h2>
+                        </>
+                    }
+                    <div className='reservationsList'>
+                        {reservations.map((reservation: ReservationTypes) => (
+                            <div className='reservationsListItem'>
+                                <p>Full Name - {reservation.first_name} {reservation.last_name}</p>
+                                <p>Email - {reservation.email}</p>
+                                <p>Date - {reservation.booking_date}</p>
+                                <p>Phone - {reservation.phone_number}</p>
+                                <p>Time - {reservation.booking_time} </p>
+                                <p>Number Of Seats - {reservation.num_of_seats}</p>
+                                <p>Special Requests - {reservation.special_request}</p>
+                            </div>
+                        ))}
                     </div>
-                    <h2 className='bookingsTitle'>Bookings Below!</h2>
-                </>
-                }
-                <div className='reservationsList'>
-                    {reservations.map((reservation: ReservationTypes) => (
-                        <div className='reservationsListItem'>
-                            <p>Full Name - {reservation.first_name} {reservation.last_name}</p>
-                            <p>Email - {reservation.email}</p>
-                            <p>Date - {reservation.booking_date}</p>
-                            <p>Phone - {reservation.phone_number}</p>
-                            <p>Time - {reservation.booking_time} </p>
-                            <p>Number Of Seats - {reservation.num_of_seats}</p>
-                            <p>Special Requests - {reservation.special_request}</p>
-                        </div>
-                    ))}
                 </div>
-            </div>
-        </main>
-        <Modal isOpen={isModalOpen} toggleModal={toggleModal}><BookTableForm/></Modal>
-        </>  
+            </main>
+            <Modal isOpen={isModalOpen} toggleModal={toggleModal}><BookTableForm /></Modal>
+        </>
     )
 }
