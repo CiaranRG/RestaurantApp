ccdd0d9555a0e846fc746cb0a703f0e55bbbfa89
Ciaran Grant <Ciarangrant2@aol.co.uk>
2024-07-24 07:00:48 +0100

    frontend loading 404 issues resolved on vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Custom output directory
+    outDir: 'dist',
     cssCodeSplit: false,
   },
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
   },
-})
+  base: './',  // Ensure this is set correctly
+});
